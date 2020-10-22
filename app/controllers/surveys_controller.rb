98a1295f7f53dcae901c6234a3de4f5a29591227
Bri Hewlett <hewlettb@chc.edu>
2020-10-21 22:18:55 -0400

    Used bootstrap and worked on create survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,5 +1,24 @@
 class SurveysController < ApplicationController
 	def index
 		@surveys = SurveyName.all
+		@scale = [1, 2, 3, 4, 5]
 	end
+
+	def new
+	end 
+
+	def create
+		survey = SurveyName.new(survey_params)
+		if survey.save
+			redirect_to "/surveys"
+		else
+			redirect_to "/surveys/new"
+		end
+	end
+
+	private
+	def survey_params
+		params.require(:survey).permit(:survey_name)
+	end
+
 end
